fe49a7fd655383c32dfa7911a09bbd5fa0a11fd0
Niwer <erwin.redote@gmail.com>
2025-09-06 00:08:35 +0200

    Optimizations, - Improved JS with async./await - Improved database by adding new projects - Improved data base by adding multiples links - Improved CSS and animations - Imrpoved skills icons (They now have the same style) - Improved responsive design

--- js/footer.inc.js
+++ js/footer.inc.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://niwer1525.github.io/Niwer1525/footer.html')
+document.addEventListener('DOMContentLoaded', async () => {
+    await fetch('https://niwer1525.github.io/Niwer1525/footer.html')
         .then(response => response.text())
-        .then(data => {
-            document.getElementById('pagefooter').innerHTML = data;
-        })
+        .then(data => document.getElementById('pagefooter').innerHTML = data)
         .catch(error => console.error('Error loading HTML:', error));
 });
